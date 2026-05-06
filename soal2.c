0ad1f08f1fda43ccf0279965f9f61f8c8ac34847
HasanSan-png <hasan.mutawakkil.al@gmail.com>
2026-05-06 09:56:29 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -33,15 +33,12 @@ int height(Node* root) {
     return (leftheight > rightheight ? leftheight : rightheight) + 1;
 }
 
-void printGivenLevel(Node* root, int level, int *first) {
+void printGivenLevel(Node* root, int level) {
     if (root == NULL) return;
-    if (level == 1) {
-        if (!(*first)) printf(" ");
-        printf("%d", root->data);
-        *first = 0;
-    } else {
-        printGivenLevel(root->left, level - 1, first);
-        printGivenLevel(root->right, level - 1, first);
+    if (level == 1) printf("%d ", root->data);
+    else if (level > 1) {
+        printGivenLevel(root->left, level - 1);
+        printGivenLevel(root->right, level - 1);
     }
 }
 
